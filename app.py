521cd0b56fcf42d73a035c88d39cb7ca153f7ee9
minhtai.pn <minhtai04082003@gmail.com>
2024-04-22 22:04:25 +0700

    Adding valid check
    valid check for unauthorized request

--- app.py
+++ app.py
@@ -9,14 +9,16 @@ expected to have permission from US Authorities.
 
 import numpy as np
 import pandas as pd
+import pickle
+import pyodbc
 from datetime import datetime
-from flask import Flask, request, render_template, Response
+from flask import Flask, request, render_template
 from flask import redirect, url_for, jsonify, make_response
 from utils.app_process import *
 from utils.database_process import *
-import pickle
-import pyodbc
-import json
+
+#global variables
+is_valid_request = False
 
 # connection string
 conn = pyodbc.connect("DRIVER={SQL Server};Server=WINDOWS-11\SQLEXPRESS;" +
@@ -25,7 +27,7 @@ conn = pyodbc.connect("DRIVER={SQL Server};Server=WINDOWS-11\SQLEXPRESS;" +
 #Load the trained model and encoder. (Pickle file)
 model = pickle.load(open('models/svr_model.pkl', 'rb'))
 scaler = pickle.load(open('models/mm_encoder.pkl', 'rb'))
-lmbda_charges = pickle.load(open('models/lmbda_price.pkl', 'rb'))
+lmbda = pickle.load(open('models/lmbda_price.pkl', 'rb'))
 
 #Flask class - Web application. 
 app = Flask(__name__)
@@ -42,6 +44,7 @@ def login():
     password = request.form.get('password')
 
     if check_login(username, password, conn):
+        global is_valid_request; is_valid_request = True
         return redirect(url_for('menu'))
     else:
         message = {'message' : 
@@ -51,7 +54,12 @@ def login():
 # menu input - prediction UI
 @app.route('/menu')
 def menu():
-    return render_template('menu.html')
+    if (is_valid_request == True):
+        return render_template('menu.html')
+    if (is_valid_request == False):
+        message = {'message' : 
+                   f'Unauthorized request'}
+        return make_response(jsonify(message), 405)
 
 # get predicted result from inputs
 @app.route('/predict',methods=['POST']) # Do not add two methods
@@ -62,26 +70,33 @@ def predict():
     bmiinput = request.form.get('bmiinput')
     childinput = request.form.get('childinput')
     smokinginput = request.form.get('smokinginput')
-    regioninput = request.form.get('regioninput')   
+    regioninput = request.form.get('regioninput')
 
+    # male and female will be transformed to 0 and 1
+    # gender_display will be used to display client's input
     gender_display = genderinput 
-    #feature transformation section
-    features = pd.DataFrame({"age":[ageinput], "sex":[genderinput], 
-                             "bmi":[bmiinput], "children":[childinput], 
-                             "smoker":[smokinginput], "region":[regioninput]}) 
     
-    features = features_transform(features)
+    #transformation section
+    features = pd.DataFrame({"age":[int(ageinput)], "sex":[genderinput], 
+                             "bmi":[float(bmiinput)], "children":[int(childinput)], 
+                             "smoker":[smokinginput], "region":[regioninput]})
     
-    features = scaler.transform(features)   
-    prediction = model.predict(features)
-    prediction = inverse_transform(prediction, lmbda_charges)
-    prediction = np.round_(prediction, 2)[0]
+    # validating section
+    if (validate_input(features) == -1):
+        message = {'message' : 
+                   f'incorrect input data'}
+        return make_response(jsonify(message), 403)
+    
+    # predicting section
+    prediction = predict_(features, scaler, model, lmbda)
 
+    # update input to database
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     features_to_database = [ageinput, genderinput, bmiinput, childinput, 
                             smokinginput, regioninput, prediction, current_time]
     insert_to_inputdata(features_to_database, conn)
 
+    global is_valid_request; is_valid_request = False
     #final section -> send data back to front page
     return render_template('index.html', age=ageinput, gender=gender_display, 
                            bmi=bmiinput, child=childinput, smoking=smokinginput, 
